35c6732320356ce8a529f32569ca5006dbba3742
Isaac Benitez <ibsandoval@gmail.com>
2023-07-22 11:09:40 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const { join, resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: { 
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      inject: "body"
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    compress: true,
+    port: 3000,
+  },
+}
